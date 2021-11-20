a551971b89d2b023e4bc8e24cb5e77b678f5deb0
Prudencio Velasco <32465853+PrudencioVelasco@users.noreply.github.com>
2021-11-20 11:51:27 -0600

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://http://143.244.185.168/api-mgseguridad';
+$config['base_url'] = 'http://143.244.185.168/api-mgseguridad';
 
 /*
 |--------------------------------------------------------------------------
